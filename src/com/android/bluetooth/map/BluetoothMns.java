lt03lte-cafaosp/platform-packages-apps-Bluetooth

commit 433ee970d746e4890a5b2c544a0815472c4b3dbf

Bluetooth: MAP removed opened cursor.
- Replaced opened cursor with collections.
- Restructured classes to use table for MAS instances.
- Restructured MAP message handle offset for multiple MAS instances.
- MAS instance for Email uses the primary email account at the time
   of connection and continue to use it for that connection
   unless it is removed.

Change-Id: I9cdbd38007cf2cf96600f030d08123a51dde06db
CRs-fixed: 311515

src/com/android/bluetooth/map/BluetoothMns.java
@@ -42,12 +42,16 @@ import android.text.format.Time;
 import android.util.Log;
 import android.util.Pair;
 
+import com.android.bluetooth.map.IBluetoothMasApp.MessageNotificationListener;
+import com.android.bluetooth.map.IBluetoothMasApp.MnsRegister;
 import com.android.bluetooth.map.MapUtils.MapUtils;
 
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.lang.reflect.Constructor;
+import java.lang.reflect.InvocationTargetException;
 import java.util.ArrayList;
 import java.util.HashSet;
 import java.util.List;
@@ -56,10 +60,14 @@ import java.util.concurrent.ConcurrentLinkedQueue;
 
 import javax.obex.ObexTransport;
 
+import static com.android.bluetooth.map.BluetoothMasService.MAS_INS_INFO;
+import static com.android.bluetooth.map.BluetoothMasService.MAX_INSTANCES;
+import static com.android.bluetooth.map.IBluetoothMasApp.HANDLE_OFFSET;
+
 /**
  * This class run an MNS session.
  */
-public class BluetoothMns {
+public class BluetoothMns implements MessageNotificationListener {
     private static final String TAG = "BtMns";
 
     private static final boolean V = BluetoothMasService.VERBOSE;
@@ -108,8 +116,7 @@ public class BluetoothMns {
 
     private EventHandler mSessionHandler;
 
-    private BluetoothMnsSmsMms bmz = null;
-    private BluetoothMnsEmail bmo = null;
+    private List<MnsClient> mMnsClients = new ArrayList<MnsClient>();
     public static final ParcelUuid BluetoothUuid_ObexMns = ParcelUuid
             .fromString("00001133-0000-1000-8000-00805F9B34FB");
 
@@ -127,8 +134,33 @@ public class BluetoothMns {
         mAdapter = BluetoothAdapter.getDefaultAdapter();
         mContext = context;
 
-        bmz = new BluetoothMnsSmsMms(mContext, this);
-        bmo = new BluetoothMnsEmail(mContext, this);
+        for (int i = 0; i < MAX_INSTANCES; i ++) {
+            try {
+                // TODO: must be updated when Class<? extends MnsClient>'s constructor is changed
+                Constructor<? extends MnsClient> constructor;
+                constructor = MAS_INS_INFO[i].mMnsClientClass.getConstructor(Context.class,
+                        Integer.class);
+                mMnsClients.add(constructor.newInstance(mContext, i));
+            } catch (IllegalArgumentException e) {
+                Log.e(TAG, "The " + MAS_INS_INFO[i].mMnsClientClass.getName()
+                        + "'s constructor arguments mismatch", e);
+            } catch (InstantiationException e) {
+                Log.e(TAG, "The " + MAS_INS_INFO[i].mMnsClientClass.getName()
+                        + " cannot be instantiated", e);
+            } catch (IllegalAccessException e) {
+                Log.e(TAG, "The " + MAS_INS_INFO[i].mMnsClientClass.getName()
+                        + " cannot be instantiated", e);
+            } catch (InvocationTargetException e) {
+                Log.e(TAG, "Exception during " + MAS_INS_INFO[i].mMnsClientClass.getName()
+                        + "'s constructor invocation", e);
+            } catch (SecurityException e) {
+                Log.e(TAG, MAS_INS_INFO[i].mMnsClientClass.getName()
+                        + "'s constructor is not accessible", e);
+            } catch (NoSuchMethodException e) {
+                Log.e(TAG, MAS_INS_INFO[i].mMnsClientClass.getName()
+                        + " has no matched constructor", e);
+            }
+        }
 
         if (!mAdapter.isEnabled()) {
             Log.e(TAG, "Can't send event when Bluetooth is disabled ");
@@ -147,33 +179,63 @@ public class BluetoothMns {
         return mSessionHandler;
     }
 
-    private void register(final int masId) {
-        new Thread(new Runnable() {
-            public void run() {
-                if (masId == 0) {
-                    bmz.register();
-                } else if (masId == 1) {
-                    bmo.register();
-                }
+    /**
+     * Asserting masId
+     * @param masId
+     * @return true if MnsClient is created for masId; otherwise false.
+     */
+    private boolean assertMasid(final int masId) {
+        final int size = mMnsClients.size();
+        if (masId < 0 || masId >= size) {
+            Log.e(TAG, "MAS id: " + masId + " is out of maximum number of MAS instances: " + size);
+            return false;
+        }
+        return true;
+    }
+
+    private boolean register(final int masId) {
+        if (!assertMasid(masId)) {
+            Log.e(TAG, "Attempt to register MAS id: " + masId);
+            return false;
+        }
+        final MnsClient client = mMnsClients.get(masId);
+        if (!client.isRegistered()) {
+            try {
+                client.register(BluetoothMns.this);
+            } catch (Exception e) {
+                Log.e(TAG, "Exception occured while register MNS for MAS id: " + masId, e);
+                return false;
             }
-        }).start();
+        }
+        return true;
     }
 
     private synchronized boolean canDisconnect() {
-        return !bmz.isRegistered() && !bmo.isRegistered();
+        for (MnsClient client : mMnsClients) {
+            if (client.isRegistered()) {
+                return false;
+            }
+        }
+        return true;
     }
 
     private void deregister(final int masId) {
-        if (masId == 0) {
-            bmz.deregister();
-        } else if (masId == 1) {
-            bmo.deregister();
+        if (!assertMasid(masId)) {
+            Log.e(TAG, "Attempt to register MAS id: " + masId);
+            return;
+        }
+        final MnsClient client = mMnsClients.get(masId);
+        if (client.isRegistered()) {
+            client.register(null);
         }
     }
 
     private void deregisterAll() {
-        bmz.deregister();
-        bmo.deregister();
+        for (MnsClient client : mMnsClients) {
+            if (client.isRegistered()) {
+                client.register(null);
+            }
+        }
     }
 
     /*
@@ -197,7 +259,10 @@ public class BluetoothMns {
                     if (mSession != null) {
                         if (V) Log.v(TAG, "is MNS session connected? " + mSession.isConnected());
                         if (mSession.isConnected()) {
-                            register(masId);
+                            if (!register(masId)) {
+                                // failed to register, disconnect
+                                obtainMessage(MNS_DISCONNECT, masId, -1).sendToTarget();
+                            }
                             break;
                         }
                     }
@@ -404,7 +469,7 @@ public class BluetoothMns {
     /**
      * Post a MNS Event to the MNS thread
      */
-    public void sendMnsEvent(String msg, String handle, String folder,
+    public void sendMnsEvent(int masId, String msg, String handle, String folder,
             String old_folder, String msgType) {
         if (V) {
             Log.v(TAG, "sendMnsEvent()");
@@ -415,7 +480,6 @@ public class BluetoothMns {
             Log.v(TAG, "msgType: " + msgType);
         }
         int location = -1;
-        int masId = -1;
 
         /* Send the notification, only if it was not initiated
          * by MCE. MEMORY_FULL and MEMORY_AVAILABLE cannot be
@@ -428,21 +492,9 @@ public class BluetoothMns {
         }
 
         if (location == -1) {
-            String str = MapUtils.mapEventReportXML(msg, handle, folder, old_folder,
-                    msgType);
-
-            if(msgType != null && msgType.equalsIgnoreCase("SMS_GSM") ||
-                        msgType.equalsIgnoreCase("MMS")){
-                Log.d(TAG, "SMS and MMS notifications sent");
-                masId = 0;
-            }
-            else if(msgType != null && msgType.equalsIgnoreCase("EMAIL")){
-                Log.d(TAG, "EMAIL notifications sent");
-                masId = 1;
-            }
-
-            mSessionHandler.obtainMessage(MNS_SEND_EVENT, masId, -1, str)
-                    .sendToTarget();
+            String str = MapUtils.mapEventReportXML(msg, handle, folder, old_folder, msgType);
+            if (V) Log.v(TAG, "Notification to MAS " + masId + ", msgType = " + msgType);
+            mSessionHandler.obtainMessage(MNS_SEND_EVENT, masId, -1, str).sendToTarget();
         } else {
             removeMceInitiatedOperation(location);
         }
@@ -504,10 +556,10 @@ public class BluetoothMns {
                 final String action = intent.getAction();
                 if (Intent.ACTION_DEVICE_STORAGE_LOW.equals(action)) {
                     Log.d(TAG, " Memory Full ");
-                    sendMnsEvent(MEMORY_FULL, null, null, null, null);
+                    sendMnsEventMemory(MEMORY_FULL);
                 } else if (Intent.ACTION_DEVICE_STORAGE_OK.equals(action)) {
                     Log.d(TAG, " Memory Available ");
-                    sendMnsEvent(MEMORY_AVAILABLE, null, null, null, null);
+                    sendMnsEventMemory(MEMORY_AVAILABLE);
                 }
             }
         }
@@ -531,11 +583,7 @@ public class BluetoothMns {
     private void startObexSession(ObexTransport transport) throws NullPointerException {
         if (V) Log.v(TAG, "Create Client session with transport " + transport.toString());
         mSession = new BluetoothMnsObexSession(mContext, transport);
-        new Thread(new Runnable() {
-            public void run() {
-                mSession.connect();
-            }
-        }).start();
+        mSession.connect();
     }
 
     private SocketConnectThread mConnectThread;
@@ -599,5 +647,83 @@ public class BluetoothMns {
             mSessionHandler.obtainMessage(RFCOMM_ERROR).sendToTarget();
             return;
         }
+    }
+
+    public void sendMnsEventMemory(String msg) {
+        // Sending "MemoryFull" or "MemoryAvailable" to all registered Mas Instances
+        for (MnsClient client : mMnsClients) {
+            if (client.isRegistered()) {
+                sendMnsEvent(client.getMasId(), msg, null, null, null, null);
+            }
+        }
+    }
+
+    public void onDeliveryFailure(int masId, String handle, String folder, String msgType) {
+        sendMnsEvent(masId, DELIVERY_FAILURE, handle, folder, null, msgType);
+    }
+
+    public void onDeliverySuccess(int masId, String handle, String folder, String msgType) {
+        sendMnsEvent(masId, DELIVERY_SUCCESS, handle, folder, null, msgType);
+    }
+
+    public void onMessageShift(int masId, String handle, String toFolder,
+            String fromFolder, String msgType) {
+        sendMnsEvent(masId, MESSAGE_SHIFT, handle, toFolder, fromFolder, msgType);
+    }
+
+    public void onNewMessage(int masId, String handle, String folder, String msgType) {
+        sendMnsEvent(masId, NEW_MESSAGE, handle, folder, null, msgType);
+    }
+
+    public void onSendingFailure(int masId, String handle, String folder, String msgType) {
+        sendMnsEvent(masId, SENDING_FAILURE, handle, folder, null, msgType);
+    }
+
+    public void onSendingSuccess(int masId, String handle, String folder, String msgType) {
+        sendMnsEvent(masId, SENDING_SUCCESS, handle, folder, null, msgType);
+    }
+
+    public void onMessageDeleted(int masId, String handle, String folder, String msgType) {
+        sendMnsEvent(masId, MESSAGE_DELETED, handle, folder, null, msgType);
+    }
+
+    public static abstract class MnsClient implements MnsRegister {
+        public static final String TAG = "MnsClient";
+        public static final boolean V = BluetoothMasService.VERBOSE;
+
+        protected Context mContext;
+        protected MessageNotificationListener mListener = null;
+        protected int mMasId;
+
+        protected final long OFFSET_START;
+        protected final long OFFSET_END;
+
+        public MnsClient(Context context, int masId) {
+            mContext = context;
+            mMasId = masId;
+            OFFSET_START = HANDLE_OFFSET[masId];
+            OFFSET_END = HANDLE_OFFSET[masId + 1] - 1;
+        }
+
+        public synchronized void register(MessageNotificationListener listener) {
+            if (listener != null) {
+                mListener = listener;
+                registerContentObserver();
+            } else {
+                mListener = null;
+                unregisterContentObserver();
+            }
+        }
+
+        public boolean isRegistered() {
+            return mListener != null;
+        }
+
+        public int getMasId() {
+            return mMasId;
+        }
+
+        protected abstract void registerContentObserver();
+        protected abstract void unregisterContentObserver();
     }
 }

src/com/android/bluetooth/map/BluetoothMasObexServer.java
@@ -841,12 +841,15 @@ public class BluetoothMasObexServer extends ServerRequestHandler {
 
     private final int msgStatus(Operation op, String name) {
         if (D) Log.d(TAG, "msgStatus: Enter");
+        if (name == null) {
+            return ResponseCodes.OBEX_HTTP_BAD_REQUEST;
+        }
         return mAppIf.msgStatus(name, masAppParams.get());
     }
 
-    private final int msgUpdate(Operation op, String name) {
+    private final int msgUpdate(Operation op) {
         if (D) Log.d(TAG, "msgUpdate: Enter");
-        return mAppIf.msgUpdate(name, masAppParams.get());
+        return mAppIf.msgUpdate();
     }
 
     private final int notification(Operation op) {
@@ -902,7 +905,7 @@ public class BluetoothMasObexServer extends ServerRequestHandler {
             return msgStatus(op, name);
         }
         if (type.equals(TYPE_MESSAGE_UPDATE)) {
-            return msgUpdate(op, name);
+            return msgUpdate(op);
         }
         if (type.equals(TYPE_MESSAGE_NOTIFICATION)) {
             return notification(op);

src/com/android/bluetooth/map/BluetoothMasService.java
@@ -47,6 +47,8 @@ import android.text.TextUtils;
 import android.util.Log;
 
 import com.android.bluetooth.R;
+import com.android.bluetooth.map.BluetoothMns.MnsClient;
+import com.android.bluetooth.map.BluetoothMnsSmsMms;
 
 import java.io.IOException;
 import java.util.ArrayList;
@@ -163,11 +165,35 @@ public class BluetoothMasService extends Service {
 
     private boolean mHasStarted = false;
     private int mStartId = -1;
+
     /**
      * The flag indicating MAP request has been notified.
      * This is set on when initiate notification and set off after accept/time out
      */
     private volatile boolean mIsRequestBeingNotified = false;
+
+    public static class MasInstanceInfo {
+        int mSupportedMessageTypes;
+        Class<? extends MnsClient> mMnsClientClass;
+        int mRfcommPort;
+
+        public MasInstanceInfo(int smt, Class<? extends MnsClient> _class, int port) {
+            mSupportedMessageTypes = smt;
+            mMnsClientClass = _class;
+            mRfcommPort = port;
+        }
+    }
+
+    public static final int MAX_INSTANCES = 2;
+    public static final MasInstanceInfo MAS_INS_INFO[] = new MasInstanceInfo[MAX_INSTANCES];
+
+    // The following information must match with corresponding
+    // SDP records supported message types and port number
+    // Please refer sdptool.c, BluetoothService.java, & init.qcom.rc
+    static {
+        MAS_INS_INFO[0] = new MasInstanceInfo(MESSAGE_TYPE_SMS_MMS, BluetoothMnsSmsMms.class, 16);
+        MAS_INS_INFO[1] = new MasInstanceInfo(MESSAGE_TYPE_EMAIL, BluetoothMnsEmail.class, 17);
+    }
 
     public BluetoothMasService() {
         mConnectionManager = new BluetoothMasObexConnectionManager();
@@ -397,11 +423,10 @@ public class BluetoothMasService extends Service {
                 new ArrayList<BluetoothMasObexConnection>();
 
         public BluetoothMasObexConnectionManager() {
-            // These connections must match with corresponding
-            // SDP records supported message types and port number
-            // Please refer sdptool.c, BluetoothService.java, & init.qcom.rc
-            mConnections.add(new BluetoothMasObexConnection(MESSAGE_TYPE_SMS_MMS, 0, 16));
-            mConnections.add(new BluetoothMasObexConnection(MESSAGE_TYPE_EMAIL, 1, 17));
+            for (int i = 0; i < MAX_INSTANCES; i ++) {
+                mConnections.add(new BluetoothMasObexConnection(
+                        MAS_INS_INFO[i].mSupportedMessageTypes, i, MAS_INS_INFO[i].mRfcommPort));
+            }
         }
 
         public void initiateObexServerSession(BluetoothDevice device) {

androidarmv6/android_packages_apps_Bluetooth_msm

commit f9bb62acb5f799e56b311046554f446821723943

Bluetooth: bug fix MAP-Event-Report
- The folder in MAP-Event-Report object shall include path.
- This bug was introduced in 433ee970d746e4890a5b2c544a0815472c4b3dbf
- Fixed race condition to disconnect MNS

Change-Id: I560a4f96b43587d20e2602682a907de023d37d5b
CRs-fixed: 320063

src/com/android/bluetooth/map/BluetoothMns.java
@@ -63,6 +63,8 @@ import javax.obex.ObexTransport;
 import static com.android.bluetooth.map.BluetoothMasService.MAS_INS_INFO;
 import static com.android.bluetooth.map.BluetoothMasService.MAX_INSTANCES;
 import static com.android.bluetooth.map.IBluetoothMasApp.HANDLE_OFFSET;
+import static com.android.bluetooth.map.IBluetoothMasApp.MSG;
+import static com.android.bluetooth.map.IBluetoothMasApp.TELECOM;
 
 /**
  * This class run an MNS session.
@@ -295,7 +297,9 @@ public class BluetoothMns implements MessageNotificationListener {
                 case RFCOMM_ERROR:
                     if (V) Log.v(TAG, "receive RFCOMM_ERROR msg");
                     deregisterAll();
-                    stop();
+                    if (canDisconnect()) {
+                        stop();
+                    }
                     break;
                 /*
                  * RFCOMM connected. Do an OBEX connect by starting the session
@@ -683,6 +687,7 @@ public class BluetoothMns implements MessageNotificationListener {
     public static abstract class MnsClient implements MnsRegister {
         public static final String TAG = "MnsClient";
         public static final boolean V = BluetoothMasService.VERBOSE;
+        protected static final String PRE_PATH = TELECOM + "/" + MSG + "/";
 
         protected Context mContext;
         protected MessageNotificationListener mListener = null;

src/com/android/bluetooth/map/BluetoothMnsEmail.java
@@ -290,10 +290,10 @@ public class BluetoothMnsEmail extends MnsClient {
                 for (EmailMessage email : values) {
                     if (V) Log.v(TAG, email.toString());
                     mListener.onNewMessage(mMasId, String.valueOf(email.mId + OFFSET_START),
-                            email.mFolderName, EMAIL);
+                            PRE_PATH + email.mFolderName, EMAIL);
                     if (email.mType == TYPE_SENT) {
                         mListener.onSendingSuccess(mMasId, String.valueOf(email.mId + OFFSET_START),
-                                email.mFolderName, EMAIL);
+                                PRE_PATH + email.mFolderName, EMAIL);
                     }
                 }
             }
@@ -306,7 +306,7 @@ public class BluetoothMnsEmail extends MnsClient {
                 for (EmailMessage email : values) {
                     if (V) Log.v(TAG, email.toString());
                     mListener.onMessageDeleted(mMasId, String.valueOf(email.mId + OFFSET_START),
-                            email.mFolderName, EMAIL);
+                            PRE_PATH + email.mFolderName, EMAIL);
                 }
             }
         }

src/com/android/bluetooth/map/BluetoothMnsSmsMms.java
@@ -274,13 +274,13 @@ public class BluetoothMnsSmsMms extends MnsClient {
                 for (Message msg : values) {
                     String folderName = (msg.mThreadId == -1) ? DELETED : msg.mFolderName;
                     mListener.onNewMessage(mMasId, String.valueOf(SMS_OFFSET_START + msg.mId),
-                            folderName, type);
+                            PRE_PATH + folderName, type);
                     if (msg.mType == Sms.MESSAGE_TYPE_SENT) {
-                        mListener.onSendingSuccess(mMasId,
-                                String.valueOf(SMS_OFFSET_START + msg.mId), SENT, type);
+                        mListener.onSendingSuccess(mMasId, String.valueOf(SMS_OFFSET_START +
+                                msg.mId), PRE_PATH + SENT, type);
                     } else if (msg.mType == Sms.MESSAGE_TYPE_FAILED) {
-                        mListener.onSendingFailure(mMasId,
-                                String.valueOf(SMS_OFFSET_START + msg.mId), OUTBOX, type);
+                        mListener.onSendingFailure(mMasId, String.valueOf(SMS_OFFSET_START +
+                                msg.mId), PRE_PATH + OUTBOX, type);
                     }
                 }
             }
@@ -296,7 +296,7 @@ public class BluetoothMnsSmsMms extends MnsClient {
                 for (Message msg : values) {
                     String folderName = (msg.mThreadId == -1) ? DELETED : msg.mFolderName;
                     mListener.onMessageDeleted(mMasId, String.valueOf(SMS_OFFSET_START + msg.mId),
-                            folderName, type);
+                            PRE_PATH + folderName, type);
                 }
             }
         }
@@ -308,10 +308,10 @@ public class BluetoothMnsSmsMms extends MnsClient {
                 for (Message msg : values) {
                     String folderName = (msg.mThreadId == -1) ? DELETED : msg.mFolderName;
                     mListener.onNewMessage(mMasId, String.valueOf(MMS_OFFSET_START + msg.mId),
-                            folderName, MMS);
+                            PRE_PATH + folderName, MMS);
                     if (msg.mType == Mms.MESSAGE_BOX_SENT) {
-                        mListener.onSendingSuccess(mMasId,
-                                String.valueOf(SMS_OFFSET_START + msg.mId), SENT, MMS);
+                        mListener.onSendingSuccess(mMasId, String.valueOf(SMS_OFFSET_START +
+                                msg.mId), PRE_PATH + SENT, MMS);
                     }
                 }
             }
@@ -324,7 +324,7 @@ public class BluetoothMnsSmsMms extends MnsClient {
                 for (Message msg : values) {
                     String folderName = (msg.mThreadId == -1) ? DELETED : msg.mFolderName;
                     mListener.onMessageDeleted(mMasId, String.valueOf(MMS_OFFSET_START + msg.mId),
-                            folderName, MMS);
+                            PRE_PATH + folderName, MMS);
                 }
             }
         }