98cb7c374401984db28e6a8f12057fa3bf9e6779
AnubhavGupta <anubhavg@codeaurora.org>
2012-01-17 17:21:41 +0530

    Bluetooth: BPP: Stop forcibly killing Connect Thread
    In some scenarios ConnectThread is still active after
    Rfcomm connection and before initiating Obex
    Connection. We should stop closing BT Socket in those
    cases.
     CRs-fixed: 329420
    
    Change-Id: Iddb7e64850f0c0241afcec16ef637f15f0379027

--- src/com/android/bluetooth/bpp/BluetoothBppTransfer.java
+++ src/com/android/bluetooth/bpp/BluetoothBppTransfer.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2011, Code Aurora Forum. All rights reserved.
+ * Copyright (c) 2011-2012, Code Aurora Forum. All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions are
@@ -329,14 +329,6 @@ public class BluetoothBppTransfer implements BluetoothOppBatch.BluetoothOppBatch
                  */
                 case RFCOMM_CONNECTED:
                     if (V) Log.v(TAG, "Transfer receive RFCOMM_CONNECTED msg checking IsAlive()");
-                    if(mConnectThread.isAlive()){
-                        try {
-                            mConnectThread.interrupt();
-                            mConnectThread.join();
-                        }catch (InterruptedException e) {
-                            if (V) Log.v(TAG, "EXP: RFCOMM_CONNECTED waiting to join");
-                        }
-                    }
                     mTransport = (ObexTransport)msg.obj;
                     startObexSession();
                     break;
