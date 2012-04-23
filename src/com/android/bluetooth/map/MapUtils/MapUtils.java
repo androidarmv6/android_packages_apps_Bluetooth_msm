cefec6fd6454837a1b7c23ea2a72c542c120532d
Ayan Ghosh <abghosh@codeaurora.org>
2012-04-23 17:38:38 +0530

    Bluetooth: MAP owner information Update.
        - Whenever phone number/name is not available from TelephonyManager
          Leave phone number field Empty and pass it to MCE.
    
        CRs-fixed: 353329
    
    Change-Id: If16bd48fa373946d4284a91d3995803991b0b17c

--- src/com/android/bluetooth/map/MapUtils/MapUtils.java
+++ src/com/android/bluetooth/map/MapUtils/MapUtils.java
@@ -1699,12 +1699,6 @@ public class MapUtils {
         }
     }
 
-    public static class ServiceUnavailableException extends Exception {
-        public ServiceUnavailableException(String reason) {
-            super("ServiceUnavailableException: " + reason);
-        }
-    }
-
     public static class RecipientVCard implements VCardInterpreter {
         String mName = "";
         String mFormattedName = "";
