2cacee97ec738a9c34087ec7216d6135704f4c78
Ayan Ghosh <abghosh@codeaurora.org>
2012-09-11 17:19:03 +0530

    Update Copyright info to retain AOSP copyright
    Change-Id: Ib8bda5964ae9340100d36144551147e7aa7f5851

--- src/com/android/bluetooth/ftp/BluetoothFtpTransport.java
+++ src/com/android/bluetooth/ftp/BluetoothFtpTransport.java
@@ -1,5 +1,6 @@
 /*
- * Copyright (c) 2011, Code Aurora Forum. All rights reserved.
+ * Copyright (c) 2008-2009, Motorola, Inc.
+ * Copyright (c) 2010-2011, Code Aurora Forum. All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions are met:
