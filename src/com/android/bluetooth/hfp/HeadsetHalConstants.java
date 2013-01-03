d9d41cfdcad4e71c640b2bdaffd81a5aeb99dafd
Ayan Ghosh <abghosh@codeaurora.org>
2013-01-03 20:39:15 +0530

    Bluetooth: Add HFP 1.6
    Change-Id: I842cea78cb352b8dcfdb908b1464dc2282a89c06

---
+++ src/com/android/bluetooth/hfp/HeadsetHalConstants.java
@@ -0,0 +1,35 @@
+/*
+ * Copyright (C) 2012 The Android Open Source Project.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.android.bluetooth.hfp;
+
+/*
+ * @hide
+ */
+
+final public class HeadsetHalConstants {
+    final static int NETWORK_STATE_NOT_AVAILABLE = 0;
+    final static int NETWORK_STATE_AVAILABLE = 1;
+    final static int SERVICE_TYPE_HOME = 0;
+    final static int SERVICE_TYPE_ROAMING = 1;
+    final static int CALL_STATE_ACTIVE = 0;
+    final static int CALL_STATE_HELD = 1;
+    final static int CALL_STATE_DIALING = 2;
+    final static int CALL_STATE_ALERTING = 3;
+    final static int CALL_STATE_INCOMING = 4;
+    final static int CALL_STATE_WAITING = 5;
+    final static int CALL_STATE_IDLE = 6;
+}
