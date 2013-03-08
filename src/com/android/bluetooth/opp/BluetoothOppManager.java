793124bf657804d7fc52f0cb762645583d9ac5a6
Smriti Gupta <smritig@codeaurora.org>
2013-03-08 12:05:46 +0530

    Display proper name while cancelling transfer
    Saves names of all the selected files
    to be transferred even before starting of client
    thread so that names remain intact in the outbound
    information even when the batch is cancelled from initiator.
    
    Change-Id: I3d6a3a0d345db6ee9a0ca2f30b1bf6361a3552ec
    CRs-fixed: 438506

--- src/com/android/bluetooth/opp/BluetoothOppManager.java
+++ src/com/android/bluetooth/opp/BluetoothOppManager.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2010-2012, The Linux Foundation. All rights reserved.
+ * Copyright (c) 2010-2013, The Linux Foundation. All rights reserved.
  * Copyright (c) 2008-2009, Motorola, Inc.
  *
  * All rights reserved.
@@ -449,11 +449,15 @@ public class BluetoothOppManager {
                     contentType = mTypeOfMultipleFiles;
                 }
 
+                BluetoothOppSendFileInfo fileInfo = BluetoothOppSendFileInfo.generateFileInfo(
+                mContext, fileUri.toString(), contentType,  mRemoteDevice.getAddress());
+
                 ContentValues values = new ContentValues();
                 values.put(BluetoothShare.URI, fileUri.toString());
                 values.put(BluetoothShare.MIMETYPE, contentType);
                 values.put(BluetoothShare.DESTINATION, mRemoteDevice.getAddress());
                 values.put(BluetoothShare.TIMESTAMP, ts);
+                values.put(BluetoothShare.FILENAME_HINT, fileInfo.mFileName);
                 if (mIsHandoverInitiated) {
                     values.put(BluetoothShare.USER_CONFIRMATION,
                             BluetoothShare.USER_CONFIRMATION_HANDOVER_CONFIRMED);
