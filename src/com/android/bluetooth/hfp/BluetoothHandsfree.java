461dd37acb49bf2bcf90935be0647488925a3626
Gubbala Venugopal Rao <vgubba@codeaurora.org>
2013-05-13 17:55:16 +0530

    Bluetooth: Send OK response to AT+CREG? command
    Found one IOT issue carkit is delaying to issue AT+CHUP command since
    it is waiting for OK response code for CREG At command send.
    
    CRs-Fixed: 486532
    Change-Id: I718097a57a82db450575317440244d4e3166bf37

--- src/com/android/bluetooth/hfp/BluetoothHandsfree.java
+++ src/com/android/bluetooth/hfp/BluetoothHandsfree.java
@@ -1,5 +1,6 @@
 /*
  * Copyright (C) 2008 The Android Open Source Project
+ * Copyright (C) 2013 The Linux Foundation. All rights reserved.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -2058,7 +2059,7 @@ public class BluetoothHandsfree {
             @Override
             public AtCommandResult handleReadCommand() {
                 updateCregState(mPhoneState.toCregString());
-                return new AtCommandResult(AtCommandResult.UNSOLICITED);
+                return new AtCommandResult(AtCommandResult.OK);
             }
         });
 
